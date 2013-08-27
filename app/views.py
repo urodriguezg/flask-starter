cc6b29a0b5efe7122b9e43f570188941c0b26ca4
Samir Makhani <makhani@berkeley.edu>
2013-08-27 11:17:16 -0700

    Integrated Flat UI, set up survey example, and created a landing page

--- app/views.py
+++ app/views.py
@@ -3,4 +3,12 @@ from flask import render_template
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html', active="home")
+
+@app.route('/example/')
+def example():
+	return render_template('example.html', active="example")
+
+@app.route('/facebook/')
+def facebook():
+	return render_template('facebook.html', active="facebook")
